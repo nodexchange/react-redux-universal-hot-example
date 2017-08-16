9afadda17e3a0f50484026c68fc7ddfc24f860fb
Digital Thorn <nodexchange@outlook.com>
2017-08-16 23:50:09 +0100

    text and the new panel

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -7,7 +7,7 @@ export default class Contact extends Component {
   panelCopyObject = () => {
     const copy = [];
     copy.push({
-      smallHeader: 'MORE THAN JUST',
+      smallHeader: 'MORE THAN JUST AN',
       header: 'AD AGENCY',
       description: 'Use our team as an extension to yours.\nOur exepertise & creativity are yours to take advantage of.\nThis is where our team excels, this is how it always should be.\nIf you have any questions or would like to pick up brain on something do not hestiate, message us now.',
       buttonText: 'Contact us',
